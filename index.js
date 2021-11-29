3e64e26ea4aac413867c229a10c344f3a2de013a
Caio <caio-cavalcanti@hotmail.com>
2021-11-29 19:12:50 -0300

    add porta

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+require("dotenv").config();
+
+const APP_PORT = process.env.PORT;
+
 
 var app = express();
+
 
 app.get('/', function(req, res) {
   res.send('Olá Mundo!');
 });
 
-app.listen(3000, function() {
+app.listen(APP_PORT, function() {
   console.log('App rodando na porta 3000!');
 });
